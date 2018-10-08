bb63cff27be060cbf99d938bcffc6e7e14b70a16
longyun <longyun@longyunMacBook-Pro.local>
2018-10-08 23:43:35 +0800

    style

--- resources/views/frontend/template-2933/component/items.blade.php
+++ resources/views/frontend/template-2933/component/items.blade.php
@@ -96,9 +96,9 @@
                 <div class="media-body">
                     <div class="clearfix">
                         @if(!empty($item->description))
-                            <article class="colo-md-12">{{{ $item->description or '' }}}</article>
+                            <article class="colo-md-12 multi-ellipsis-3">{{{ $item->description or '' }}}</article>
                         @else
-                            <article class="colo-md-12">{!! $item->content_show or '' !!}</article>
+                            <article class="colo-md-12 multi-ellipsis-3">{!! $item->content_show or '' !!}</article>
                         @endif
 
                     </div>
